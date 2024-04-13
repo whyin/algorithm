14011560fdbbbc8d6ff5a7b580e8d15d60475444
whyin <130069178+whyin@users.noreply.github.com>
2024-04-13 17:37:55 +0900

    [Bronze II] Title: 공 바꾸기, Time: 44 ms, Memory: 31252 KB -BaekjoonHub

---
+++ 백준/Bronze/10813. 공 바꾸기/공 바꾸기.py
@@ -0,0 +1,14 @@
+N, M = map(int, input().split())
+
+basket = [i for i in range(1,N+1)]
+temp = 0
+
+for _ in range(M):
+    i, j = map(int, input().split())
+    temp = basket[i-1]
+    basket[i-1] = basket[j-1]
+    basket[j-1] = temp
+
+for i in range(N):
+    print(basket[i], end= " ")
+    
