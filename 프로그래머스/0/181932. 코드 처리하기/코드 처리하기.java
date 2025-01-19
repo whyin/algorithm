a1fc04ba55d10e754bd1956c117859945cf70ca1
whyin <130069178+whyin@users.noreply.github.com>
2025-01-19 23:44:34 +0900

    [level 0] Title: 코드 처리하기, Time: 6.34 ms, Memory: 90.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/181932. 코드 처리하기/코드 처리하기.java
@@ -0,0 +1,18 @@
+class Solution {
+    public String solution(String code) {
+        StringBuilder answer = new StringBuilder();
+        int mode = 0;
+        for (int i = 0; i < code.length(); i++) {
+            char current = code.charAt(i);
+            if (current == '1') {
+                mode = mode == 0 ? 1 : 0;
+                continue;
+            }
+
+            if (i % 2 == mode) {
+                answer.append(current);
+            }
+        }
+        return answer.length() == 0 ? "EMPTY" : answer.toString();
+    }
+}
