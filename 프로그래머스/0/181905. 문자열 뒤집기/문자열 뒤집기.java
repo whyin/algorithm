b966851ef0b03df0b9f64dacba50950190784431
whyin <130069178+whyin@users.noreply.github.com>
2025-04-27 20:18:45 +0900

    [level 0] Title: 문자열 뒤집기, Time: 14.11 ms, Memory: 78.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181905. 문자열 뒤집기/문자열 뒤집기.java
@@ -0,0 +1,7 @@
+class Solution {
+    public static String solution(String my_string, int s, int e) {
+        StringBuilder answer = new StringBuilder(my_string.substring(s, e+1));
+        answer.reverse();
+        return my_string.substring(0,s) + answer + my_string.substring(e+1);
+    }
+}
