f6db01996b3a631c3446c829bb28782c96280345
whyin <130069178+whyin@users.noreply.github.com>
2025-04-27 19:35:26 +0900

    [level 0] Title: 문자열의 뒤의 n글자, Time: 0.02 ms, Memory: 93.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181910. 문자열의 뒤의 n글자/문자열의 뒤의 n글자.java
@@ -0,0 +1,8 @@
+class Solution {
+    public String solution(String my_string, int n) {
+        int length = my_string.length();
+        String answer = my_string.substring(length - n);
+    
+        return answer;
+    }
+}
