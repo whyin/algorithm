b2c0460f9138f040cc458ca4b53b71c9b9f9a43c
whyin <130069178+whyin@users.noreply.github.com>
2025-04-30 16:14:19 +0900

    [level 0] Title: 할 일 목록, Time: 0.05 ms, Memory: 70.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181885. 할 일 목록/할 일 목록.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+import java.util.List;
+
+class Solution {
+    public String[] solution(String[] todo_list, boolean[] finished) {
+        List<String> list = new ArrayList<>();
+
+        for (int i = 0; i < finished.length; i++) {
+            if (!finished[i]) {
+                list.add(todo_list[i]);
+            }
+        }
+        String[] answer = new String[list.size()];
+        return list.toArray(answer);
+    }
+}
