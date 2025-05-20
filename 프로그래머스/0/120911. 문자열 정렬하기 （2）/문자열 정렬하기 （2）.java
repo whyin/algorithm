447cc6da64fd808d66529db581c4ac7a59fcc181
whyin <130069178+whyin@users.noreply.github.com>
2025-05-20 20:36:22 +0900

    [level 0] Title: 문자열 정렬하기 (2), Time: 0.51 ms, Memory: 77.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120911. 문자열 정렬하기 （2）/문자열 정렬하기 （2）.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+class Solution {
+    public String solution(String my_string) {
+        char[] charArray = my_string.toLowerCase().toCharArray();
+        Arrays.sort(charArray);
+        return new String(charArray);
+    }
+}
