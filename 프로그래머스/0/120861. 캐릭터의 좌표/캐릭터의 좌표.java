c8c92839b89e3e08210ca9c2a0f3c163a480b31e
whyin <130069178+whyin@users.noreply.github.com>
2025-05-21 13:48:29 +0900

    [level 0] Title: 캐릭터의 좌표, Time: 0.03 ms, Memory: 86.9 MB -BaekjoonHub

--- 프로그래머스/0/120861. 캐릭터의 좌표/캐릭터의 좌표.java
+++ 프로그래머스/0/120861. 캐릭터의 좌표/캐릭터의 좌표.java
@@ -1,16 +1,18 @@
 class Solution {
     public int[] solution(String[] keyinput, int[] board) {
-        int x = 0, y = 0;
+        int x = 0;
+        int y = 0;
+
         int maxX = board[0] / 2;
         int maxY = board[1] / 2;
 
         for (String key : keyinput) {
-            switch (key) {
+            switch (key){
                 case "up":
                     if (y + 1 <= maxY) y++;
                     break;
                 case "down":
-                    if (y - 1 >= -maxY) y--;
+                    if (y - 1 >= -maxY ) y--;
                     break;
                 case "left":
                     if (x - 1 >= -maxX) x--;
@@ -20,7 +22,6 @@ class Solution {
                     break;
             }
         }
-
         return new int[]{x, y};
     }
 }
