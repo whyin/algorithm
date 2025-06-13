965de5584612b0ed362b249f9da437c3773b535d
whyin <130069178+whyin@users.noreply.github.com>
2025-06-13 14:41:53 +0900

    [level 1] Title: 자릿수 더하기, Time: 0.19 ms, Memory: 77.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class Solution {
+    public int solution(int n) {
+        int answer = 0;
+
+        String[] str = (String.valueOf(n)).split("");
+        for (String s : str) {
+            answer += Integer.parseInt(s);
+        }
+        return answer;
+    }
+}
