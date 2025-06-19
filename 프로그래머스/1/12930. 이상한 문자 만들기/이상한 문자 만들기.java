21999c9b12b4a539085b2396d38751fc398ea7fd
whyin <130069178+whyin@users.noreply.github.com>
2025-06-19 16:18:31 +0900

    [level 1] Title: 이상한 문자 만들기, Time: 0.11 ms, Memory: 87.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.java
@@ -0,0 +1,23 @@
+class Solution {
+    public String solution(String s) {
+        StringBuilder sb = new StringBuilder();
+         int index = 0;
+        
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            
+            if (c == ' ') {
+                sb.append(c);
+                index = 0;
+            } else {
+                if (index % 2 == 0) {
+                sb.append(Character.toUpperCase(c));
+                } else {
+                sb.append(Character.toLowerCase(c));
+                }
+                index++;
+            }
+        }
+        return sb.toString();
+    }
+}
