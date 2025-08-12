51c3dee8248fb70fdc408b4b9d9873cb0e18ba67
whyin <130069178+whyin@users.noreply.github.com>
2025-08-12 21:17:53 +0900

    [level 1] Title: 핸드폰 번호 가리기, Time: 1.82 ms, Memory: 83.1 MB -BaekjoonHub

--- 프로그래머스/1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.java
+++ 프로그래머스/1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.java
@@ -1,8 +1,6 @@
+
 class Solution {
     public String solution(String phone_number) {
-        int len = phone_number.length();
-        String star = "*".repeat(len - 4);
-        String last4 = phone_number.substring(len - 4);
-        return star + last4;
+        return "*".repeat(phone_number.length()-4) + phone_number.substring(phone_number.length()-4);
     }
 }
