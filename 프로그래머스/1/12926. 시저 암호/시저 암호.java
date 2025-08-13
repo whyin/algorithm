37a6db55ef9ec20c65672323f89891e8e3f0a3fa
whyin <130069178+whyin@users.noreply.github.com>
2025-08-13 21:30:04 +0900

    [level 1] Title: 시저 암호, Time: 26.92 ms, Memory: 102 MB -BaekjoonHub

--- 프로그래머스/1/12926. 시저 암호/시저 암호.java
+++ 프로그래머스/1/12926. 시저 암호/시저 암호.java
@@ -1,16 +1,16 @@
-class Solution {
+class Solution {    
     public String solution(String s, int n) {
-        // 아스키코드로 char
         // Z일 경우 고려 (a: 97; z: 122, A: 65; Z: 90) 
-        
-        String answer = "";
+        String result = "";
         for (int i = 0; i < s.length(); i++) {
-            if (s.charAt(i) == ' '){answer += " ";} else if ((int) s.charAt(i) <= 90) {
-                answer += (int) s.charAt(i) + n <= 90 ? (char) (s.charAt(i) + n) : (char)( s.charAt(i) + (n - 26));
+            if (s.charAt(i) == ' ') {
+                result += " ";
+            } else if ((int) s.charAt(i) <= 90) {
+                result += (int) s.charAt(i) + n <= 90 ? (char) (s.charAt(i) + n) : (char) (s.charAt(i) + (n - 26)); 
             } else {
-                answer += (int) s.charAt(i) + n <= 122 ? (char) (s.charAt(i) + n) : (char) (s.charAt(i) + (n - 26));
+                result += (int) s.charAt(i) + n <= 122 ? (char) (s.charAt(i) + n) : (char) (s.charAt(i) + (n - 26));
             }
         }
-        return answer;
+        return result;
     }
 }
