ee29d97678f303d97ac2ae029fd978e740ca49cd
whyin <130069178+whyin@users.noreply.github.com>
2025-08-16 02:08:15 +0900

    [level 1] Title: 달리기 경주, Time: 272.64 ms, Memory: 362 MB -BaekjoonHub

--- 프로그래머스/1/178871. 달리기 경주/달리기 경주.java
+++ 프로그래머스/1/178871. 달리기 경주/달리기 경주.java
@@ -2,28 +2,27 @@ import java.util.*;
 
 class Solution {
     public String[] solution(String[] players, String[] callings) {
-        // 1. 맵으로 이름, 순위 저장
-        // 2. callings과 말 이름 비교 후 순위 하나씩 밀기
+        String[] result = new String[players.length];
+        
         Map<String, Integer> map = new HashMap<>();
+        
         
         for (int i = 0; i < players.length; i++) {
             map.put(players[i], i);
-        }
+        } 
         
         for (String name : callings) {
-            int index = map.get(name); // 추월한 말 번호
-            if(index == 0) continue;
+            int index = map.get(name); // 현재 몇등인지 
+            if (index == 0 ) continue;
             
             String front = players[index - 1];
             
             players[index - 1] = name;
             players[index] = front;
             
-            map.put(name, index -1);
+            map.put(name, index - 1);
             map.put(front, index);
-            
         }
-        
         return players;
     }
 }
