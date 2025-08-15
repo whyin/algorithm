6ea3d7be7f8ffcc32f2dfc503926b585a1376fad
whyin <130069178+whyin@users.noreply.github.com>
2025-08-16 01:17:57 +0900

    [level 1] Title: [1차] 비밀지도, Time: 9.24 ms, Memory: 87.5 MB -BaekjoonHub

--- 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.java
+++ 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.java
@@ -1,16 +1,14 @@
 class Solution {
     public String[] solution(int n, int[] arr1, int[] arr2) {
-        String[] answer = new String[n];
-    
+        String[] arr = new String[n];
+        
         for (int i = 0; i < n; i++) {
-            
             int merged = arr1[i] | arr2[i];
-            String step1 = String.format("%" + n + "s", Integer.toBinaryString(merged).replace(' ', '0'));
+            String step1 = String.format("%" + n + "s", Integer.toBinaryString(merged)); 
             
-            String step2 = step1.replaceAll("1", "#").replaceAll("0", " ");
-            
-            answer[i] = step2;
+            String step2 = step1.replaceAll("0"," ").replaceAll("1", "#");
+            arr[i] = step2;
         }
-        return answer;
+        return arr;
     }
 }
