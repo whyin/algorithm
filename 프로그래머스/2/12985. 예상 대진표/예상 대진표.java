53434a95f37e248ef26bf8716cc4688dc8a738e9
whyin <130069178+whyin@users.noreply.github.com>
2025-08-16 01:22:55 +0900

    [level 2] Title: 예상 대진표, Time: 0.03 ms, Memory: 87.7 MB -BaekjoonHub

--- 프로그래머스/2/12985. 예상 대진표/예상 대진표.java
+++ 프로그래머스/2/12985. 예상 대진표/예상 대진표.java
@@ -2,31 +2,11 @@ class Solution {
     public int solution(int n, int a, int b) {
         int answer = 0;
 
-        /* 
-        4 -> 2 -> 1 
-        7 -> 4 -> 2 -> 1
-        */
-        
-        
-        while(true) {
-            if (a == b) break;
-            
-            if (a != 1) {
-                if (a % 2 == 1) {
-                    a += 1;
-                }
-                a /= 2;
-            }
-            
-            if (b != 1) {
-                if (b % 2 == 1) {
-                    b += 1;
-                }
-                b /= 2;
-            }
+        while(a != b) {
+            a = a/2 + a%2; // 4 -> 2 -> 1
+            b = b/2 + b%2; // 7 -> 4 -> 2 -> 1
             answer++;
         }        
-
         return answer;
     }
 }
