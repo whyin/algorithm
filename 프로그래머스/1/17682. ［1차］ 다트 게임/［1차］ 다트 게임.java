3f7b03b5d4099feaf3640f16120ca853d5e54f3e
whyin <130069178+whyin@users.noreply.github.com>
2025-08-16 12:14:48 +0900

    [level 1] Title: [1차] 다트 게임, Time: 0.30 ms, Memory: 76.9 MB -BaekjoonHub

--- 프로그래머스/1/17682. ［1차］ 다트 게임/［1차］ 다트 게임.java
+++ 프로그래머스/1/17682. ［1차］ 다트 게임/［1차］ 다트 게임.java
@@ -12,35 +12,38 @@ class Solution {
         
         while(m.find()) {
             String num = m.group(1);
-            String pow = m.group(2);
-            String opt = m.group(3);
-            rounds.add(new String[]{num, pow, opt});
+            String bonus = m.group(2);
+            String option = m.group(3);
+            rounds.add(new String[]{num, bonus, option});
         }
         
         for (int i = 0; i < rounds.size(); i++) {
-            String[] r = rounds.get(i);
+            String[] round = rounds.get(i);
             
             int num = 0;
-            int a = Integer.parseInt(r[0]);
-            if (r[1].equals("S")) {
+            int a = Integer.parseInt(round[0]);
+            
+            if (round[1].equals("S")) {
                 num += a;
-            } else if (r[1].equals("D")) {
+            } else if (round[1].equals("D")) {
                 num += a * a;
-            } else if (r[1].equals("T")) {
+            } else if (round[1].equals("T")) {
                 num += a * a * a;
             }
             
-            
-            if (r[2].equals("*")) {
+            if (round[2].equals("*")) {
                 num *= 2;
                 if (i > 0) {
-                    result[i - 1] *= 2;
+                    result[i-1] *= 2; 
                 }
-            } else if (r[2].equals("#")) {
+            } else if (round[2].equals("#")) {
                 num = -num;
             }
+            
             result[i] = num;
+            
         }
+        
         return result[0] + result[1] + result[2];
     }
 }
