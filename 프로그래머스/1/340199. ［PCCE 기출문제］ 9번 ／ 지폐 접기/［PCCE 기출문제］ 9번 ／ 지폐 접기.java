72d91c6161d55dffb2324bd65ea217247291379d
whyin <130069178+whyin@users.noreply.github.com>
2025-08-24 14:51:04 +0900

    [level 1] Title: [PCCE 기출문제] 9번 / 지폐 접기, Time: 0.07 ms, Memory: 80.6 MB -BaekjoonHub

--- 프로그래머스/1/340199. ［PCCE 기출문제］ 9번 ／ 지폐 접기/［PCCE 기출문제］ 9번 ／ 지폐 접기.java
+++ 프로그래머스/1/340199. ［PCCE 기출문제］ 9번 ／ 지폐 접기/［PCCE 기출문제］ 9번 ／ 지폐 접기.java
@@ -2,19 +2,22 @@ import java.util.*;
 
 class Solution {
     public int solution(int[] wallet, int[] bill) {
-        int wMin = Math.min(wallet[0], wallet[1]);
-        int wMax = Math.max(wallet[0], wallet[1]);
-
-        long a = bill[0];   // 지폐 가로
-        long b = bill[1];   // 지폐 세로
-        int folds = 0;
-
-        // 회전 가능하므로 (작은 변 ≤ 지갑 작은 변) && (큰 변 ≤ 지갑 큰 변) 이면 종료
-        while (!(Math.min(a, b) <= wMin && Math.max(a, b) <= wMax)) {
-            if (a >= b) a /= 2;     // 긴 변만 접기
-            else        b /= 2;
-            folds++;
+        int max = Math.max(wallet[0], wallet[1]);
+        int min = Math.min(wallet[0], wallet[1]);
+        
+        int count = 0;
+        
+        int a = bill[0];
+        int b = bill[1];
+        
+        while (!(Math.max(a,b) <= max && Math.min(a,b) <= min)) {
+            if (a >= b) a /= 2;
+            else b /=2;
+            
+            count++;
         }
-        return folds;
+        
+        return count;
+        
     }
 }
