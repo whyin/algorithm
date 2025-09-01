1fb009d3a9b88cdefe67a77166b7a48bafa5f3d6
whyin <130069178+whyin@users.noreply.github.com>
2025-09-01 16:35:57 +0900

    [level 0] Title: 문자 개수 세기, Time: 0.12 ms, Memory: 74 MB -BaekjoonHub

--- 프로그래머스/0/181902. 문자 개수 세기/문자 개수 세기.java
+++ 프로그래머스/0/181902. 문자 개수 세기/문자 개수 세기.java
@@ -1,15 +1,15 @@
 class Solution {
    public int[] solution(String my_string) {
-        int[] answer = new int[52];
-        for (int i = 0; i < my_string.length(); i++) {
-            char c = my_string.charAt(i);
+       int[] answer = new int[52];
+       
+       for (char c : my_string.toCharArray()) {
+           if ('A' <= c && c <= 'Z') {
+               answer[c - 'A']++;
+           } else if ('a' <= c && c <= 'z') {
+               answer[26 + c - 'a']++;
+           }
+       }
+       return answer;
 
-            if (c >= 'A' && c <= 'Z') {
-                answer[c - 'A']++;
-            } else if (c >= 'a' && c <= 'z') {
-                answer[26 + c - 'a']++;
-            }
-        }
-        return answer;
     }
 }
