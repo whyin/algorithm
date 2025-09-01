0bc9ee6c1834e6a6465e26afcae2a4817d1457a0
whyin <130069178+whyin@users.noreply.github.com>
2025-09-01 17:42:22 +0900

    [level 0] Title: 짝수는 싫어요, Time: 2.95 ms, Memory: 74.3 MB -BaekjoonHub

--- 프로그래머스/0/120813. 짝수는 싫어요/짝수는 싫어요.java
+++ 프로그래머스/0/120813. 짝수는 싫어요/짝수는 싫어요.java
@@ -5,14 +5,10 @@ class Solution {
     public int[] solution(int n) {
         List<Integer> list = new ArrayList<>();
 
-        for (int i = 1; i <= n; i += 2) {
+        for (int i = 1; i <= n; i+=2) {
             list.add(i);
         }
-        int[] answer = new int[list.size()];
-        for (int i = 0; i < list.size(); i++) {
-            answer[i] = list.get(i);
-        }
         
-        return answer;
+        return list.stream().mapToInt(Integer::intValue).toArray();
     }
 }
