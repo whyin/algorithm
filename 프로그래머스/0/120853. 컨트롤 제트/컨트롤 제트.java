cf12ec132d6cb7afb17a541e10fdb357d2dedcb2
whyin <130069178+whyin@users.noreply.github.com>
2025-09-02 16:42:45 +0900

    [level 0] Title: 컨트롤 제트, Time: 2.01 ms, Memory: 85.8 MB -BaekjoonHub

--- 프로그래머스/0/120853. 컨트롤 제트/컨트롤 제트.java
+++ 프로그래머스/0/120853. 컨트롤 제트/컨트롤 제트.java
@@ -1,22 +1,16 @@
 import java.util.*;
+
 class Solution {
     public int solution(String s) {
-        String[] tokens = s.split(" ");
-        int sum = 0;
         Stack<Integer> stack = new Stack<>();
-
-        for (String token : tokens) {
-            if (token.equals("Z")) {
-                if (!stack.isEmpty()) {
-                    sum -= stack.pop();  // 바로 전 숫자 제거
-                }
-            } else {
-                int num = Integer.parseInt(token);
-                sum += num;
-                stack.push(num);  // 되돌릴 수 있도록 저장
+        
+        for (String str : s.split(" ")) {
+            if (str.equals("Z") && !stack.isEmpty()) stack.pop();
+            else {
+                stack.push(Integer.parseInt(str));
             }
         }
-
-        return sum;
+        return stack.stream().mapToInt(Integer::intValue).sum();
+        
     }
 }
