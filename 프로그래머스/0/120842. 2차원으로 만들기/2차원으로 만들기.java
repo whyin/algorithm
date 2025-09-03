730fc7900fc8669d523f0f0411865189b760b5c3
whyin <130069178+whyin@users.noreply.github.com>
2025-09-03 18:56:34 +0900

    [level 0] Title: 2차원으로 만들기, Time: 0.03 ms, Memory: 91.1 MB -BaekjoonHub

--- 프로그래머스/0/120842. 2차원으로 만들기/2차원으로 만들기.java
+++ 프로그래머스/0/120842. 2차원으로 만들기/2차원으로 만들기.java
@@ -1,15 +1,17 @@
 class Solution {
     public int[][] solution(int[] num_list, int n) {
-        int num = num_list.length / n;
-        int[][] answer = new int[num][n];
-        int a = 0;
+        int len = num_list.length / n;
         
-        for (int i = 0; i < num; i++) {
+        int[][] arr = new int[len][n];
+        
+        int index = 0;
+        
+        for (int i = 0; i < len; i++) {
             for (int j = 0; j < n; j++) {
-                answer[i][j] = num_list[a];
-                a++;
+                arr[i][j] = num_list[index];
+                index++;
             }
         }
-        return answer;
+        return arr;
     }
 }
