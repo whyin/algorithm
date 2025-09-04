a5eee6ff2e0ad945c8cc5a619d81c0e6f8554ceb
whyin <130069178+whyin@users.noreply.github.com>
2025-09-04 17:07:50 +0900

    [level 1] Title: 예산, Time: 0.52 ms, Memory: 79.9 MB -BaekjoonHub

--- 프로그래머스/1/12982. 예산/예산.java
+++ 프로그래머스/1/12982. 예산/예산.java
@@ -2,19 +2,16 @@ import java.util.*;
 
 class Solution {
     public int solution(int[] d, int budget) {
+        Arrays.sort(d);
+        int result = 0;
         
-        Arrays.sort(d); // 오름차순 정렬
-        int count = 0;   // 지원해 줄 수 있는 부서 수
-
         for (int cost : d) {
             if (budget - cost >= 0) {
                 budget -= cost;
-                count++;
-            } else {
-                break; // 예산이 부족하면 반복 종료
-            }
+                result++;
+            } else break;
         }
-
-        return count;
+        return result;
+        
     }
 }
