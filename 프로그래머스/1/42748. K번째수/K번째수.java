9564b05a06404bebf13c5142fe6f6b3b982953bd
whyin <130069178+whyin@users.noreply.github.com>
2025-09-04 16:46:41 +0900

    [level 1] Title: K번째수, Time: 0.38 ms, Memory: 90.2 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.java
+++ 프로그래머스/1/42748. K번째수/K번째수.java
@@ -5,11 +5,13 @@ class Solution {
         int[] answer = new int[commands.length];
         
         for (int i = 0; i < commands.length; i++) {
-            int[] temp = Arrays.copyOfRange(array, commands[i][0] - 1, commands[i][1]);
-            Arrays.sort(temp);
-            int n = commands[i][2] - 1;
-            answer[i] = temp[n];
+            int[] arr = Arrays.copyOfRange(array, 
+                                           commands[i][0] - 1,
+                                           commands[i][1]);
+            Arrays.sort(arr);
+            answer[i] = arr[commands[i][2] - 1];
         }
+        
         return answer;
     }
 }
