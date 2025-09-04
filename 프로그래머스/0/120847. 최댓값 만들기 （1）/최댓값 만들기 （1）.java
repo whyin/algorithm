c2a5dc823d890ad34c43bdcfcc09b68a049a2870
whyin <130069178+whyin@users.noreply.github.com>
2025-09-04 16:23:09 +0900

    [level 0] Title: 최댓값 만들기 (1), Time: 0.05 ms, Memory: 89.3 MB -BaekjoonHub

--- 프로그래머스/0/120847. 최댓값 만들기 （1）/최댓값 만들기 （1）.java
+++ 프로그래머스/0/120847. 최댓값 만들기 （1）/최댓값 만들기 （1）.java
@@ -1,8 +1,18 @@
 import java.util.Arrays;
+
 class Solution {
     public int solution(int[] numbers) {
-        Arrays.sort(numbers);
-
-        return numbers[numbers.length - 1] * numbers[numbers.length - 2];
+        int max1 = Math.max(numbers[0], numbers[1]);
+        int max2 = Math.min(numbers[0], numbers[1]);
+        
+        for (int i = 2; i < numbers.length; i++) {
+            if (numbers[i] > max1) {
+                max2 = max1;
+                max1 = numbers[i];
+            } else if (numbers[i] > max2) {
+                max2 = numbers[i];
+            }
+        }
+        return max1 * max2;
     }
 }
