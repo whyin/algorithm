db29e442a6d0950981ababf82244e8a6661b2e68
whyin <130069178+whyin@users.noreply.github.com>
2025-09-04 16:35:12 +0900

    [level 0] Title: 가까운 수, Time: 0.05 ms, Memory: 73 MB -BaekjoonHub

--- 프로그래머스/0/120890. 가까운 수/가까운 수.java
+++ 프로그래머스/0/120890. 가까운 수/가까운 수.java
@@ -2,18 +2,20 @@ import java.util.*;
 
 class Solution {
     public static int solution(int[] array, int n) {
-        int closest = array[0];
-        int minDiff = Math.abs(array[0] - n);
-
+        // 초기값
+        int diff = Math.abs(array[0] - n);
+        int answer = array[0];
+        
         for (int i = 1; i < array.length; i++) {
-            int diff = Math.abs(array[i] - n);
-
-            if (diff < minDiff || (diff == minDiff && array[i] < closest)) {
-                minDiff = diff;
-                closest = array[i];
+            
+            if (diff > Math.abs(array[i] - n)) {
+                diff = Math.abs(array[i] - n);
+                answer = array[i];
+            } else if (diff == Math.abs(array[i] - n)) {
+                answer = Math.min(answer, array[i]);
             }
+            
         }
-
-        return closest;
+        return answer;
     }
 }
