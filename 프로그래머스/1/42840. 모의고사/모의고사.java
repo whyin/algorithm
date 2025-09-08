02b3b0ff87f954585d4fbffc1b85c540ce023af4
whyin <130069178+whyin@users.noreply.github.com>
2025-09-08 16:53:23 +0900

    [level 1] Title: 모의고사, Time: 3.97 ms, Memory: 79.5 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.java
+++ 프로그래머스/1/42840. 모의고사/모의고사.java
@@ -7,28 +7,29 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int[] answers) {
-        
         int[] arr1 = {1, 2, 3, 4, 5};
         int[] arr2 = {2, 1, 2, 3, 2, 4, 2, 5};
         int[] arr3 = {3, 3, 1, 1, 2, 2, 4, 4, 5, 5};
-        
-        int countArr1 = 0;
-        int countArr2 = 0;
-        int countArr3 = 0;
-        
-        for (int i = 0; i < answers.length; i++) {
-            if (arr1[i % arr1.length] == answers[i % answers.length]) countArr1++;
-            if (arr2[i % arr2.length] == answers[i % answers.length]) countArr2++;
-            if (arr3[i % arr3.length] == answers[i % answers.length]) countArr3++;
+
+        int count1 = 0;
+        int count2 = 0;
+        int count3 = 0;
+
+        int len = answers.length;
+
+        for (int i = 0; i < len; i++) {
+            if (arr1[i % arr1.length] == answers[i]) count1++;
+            if (arr2[i % arr2.length] == answers[i]) count2++;
+            if (arr3[i % arr3.length] == answers[i]) count3++;
         }
-        
-        int max = Math.max(Math.max(countArr1, countArr2), countArr3);
-        
+
+        int max = Math.max(Math.max(count1, count2), count3);
+
         List<Integer> list = new ArrayList<>();
-        if (countArr1 == max) list.add(1);
-        if (countArr2 == max) list.add(2);
-        if (countArr3 == max) list.add(3);
-        
+        if (max == count1) list.add(1);
+        if (max == count2) list.add(2);
+        if (max == count3) list.add(3);
+
         return list.stream().mapToInt(Integer::intValue).toArray();
     }
 }
