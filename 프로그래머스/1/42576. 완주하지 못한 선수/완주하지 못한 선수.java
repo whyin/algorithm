c6ed41832332a6e69cc6ff69b3bea71c9fcf5daa
whyin <130069178+whyin@users.noreply.github.com>
2025-09-09 17:23:21 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 83.65 ms, Memory: 90.2 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -3,17 +3,19 @@ import java.util.*;
 class Solution {
     public String solution(String[] participant, String[] completion) {
         Map<String, Integer> map = new HashMap<>();
-        
+
         for (String s : participant) {
             map.put(s, map.getOrDefault(s, 0) + 1);
         }
-        
+
         for (String s : completion) {
             map.put(s, map.get(s) - 1);
         }
-        
+
         for (String s : map.keySet()) {
-            if (map.get(s) == 1) return s;
+            if (map.get(s) != 0) {
+                return s;
+            }
         }
         return null;
     }
