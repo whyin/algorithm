f181261a57c2f294007caf8dd785cf9954d76776
whyin <130069178+whyin@users.noreply.github.com>
2025-09-09 17:44:34 +0900

    [level 1] Title: 폰켓몬, Time: 4.28 ms, Memory: 76.3 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
@@ -3,11 +3,10 @@ import java.util.*;
 class Solution {
     public int solution(int[] nums) {
         Set<Integer> set = new HashSet<>();
-        
-        for (int n : nums) {
-            set.add(n);
+        for (int num : nums) {
+            set.add(num);
         }
-        
+
         return set.size() >= nums.length / 2 ? nums.length / 2 : set.size();
     }
 }
